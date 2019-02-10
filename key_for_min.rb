3483781c2272cc11e5bbeaf33ecc3d5266be3619
Katie Blizzard <kblizzard11@gmail.com>
2019-02-10 17:40:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  if name_hash.empty?
+    return nil 
+  else 
+  
   name_hash.each do |key, value|
     
     array = name_hash.collect do |key2, value2| 
@@ -21,5 +26,6 @@ def key_for_min_value(name_hash)
   end 
     
   
-  end 
+  end
+end
 end
