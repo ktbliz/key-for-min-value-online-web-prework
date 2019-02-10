7500aeecf8ded83fc3f4aeb54c89a8591f4d8b6b
Katie Blizzard <kblizzard11@gmail.com>
2019-02-10 17:28:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,11 @@
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     
+    array = name_hash.collect do |key2, value2| 
+      array << value2 
+    end
     
-    if value <= name_hash.collect do |key2, value2| value2 end
+    if value <= 
       return key 
     end 
   
